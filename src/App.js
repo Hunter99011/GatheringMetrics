9cbee11df802aff90d690f2a7c637eb97bfceec3
Hunter99011 <hunter.99011@gmail.com>
2024-07-06 00:33:57 +0200

    Basic Layout for routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
+import Analytics from "./Analytics";
+import Management from "./Management";
+import Settings from "./Settings";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Login/>}/>
             <Route path="/home" element={<Home/>}/>
+            <Route path="/analytics" element={<Analytics/>}/>
+            <Route path="/management" element={<Management/>}/>
+            <Route path="/settings" element={<Settings/>}/>
             <Route path="*" element={<NotFound/>}/>
           </Routes>
         </div>
